aaf45b1645b7343d378260368f188203484c6987
kwaknet01 <kwaknet01@gmail.com>
2019-11-17 07:54:47 +0800

    Update pihotspot.sh

--- pihotspot.sh
+++ pihotspot.sh
@@ -82,7 +82,7 @@ KUPIKI_ALLOW_REGISTER=Y
 # *************************************
 
 # Current script version
-KUPIKI_VERSION="2.1.3"
+KUPIKI_VERSION="2.1.1"
 # Updater location
 KUPIKI_UPDATER_ARCHIVE="https://raw.githubusercontent.com/pihomeserver/Kupiki-Hotspot-Script/master/kupiki_updater.sh"
 # Default Portal port
@@ -94,8 +94,6 @@ if [ "$HOTSPOT_HTTPS" = "Y" ]; then
     HOTSPOT_PROTOCOL="https:\/\/"
 fi
 
-# Minimal version for Debian
-DEBIAN_MINIMAL_VERSION=9.0
 # Default version of MariaDB
 MARIADB_VERSION='10.1'
 # CoovaChilli GIT URL
@@ -354,8 +352,7 @@ package_check_install() {
 
 PIHOTSPOT_DEPS_START=( apt-transport-https localepurge git wget )
 PIHOTSPOT_DEPS_WIFI=( apt-utils firmware-brcm80211 firmware-ralink firmware-realtek )
-#PIHOTSPOT_DEPS=( build-essential grep whiptail debconf-utils nfdump figlet git fail2ban hostapd php-mysql php-pear php-gd php-db php-fpm libgd-dev libpcrecpp0v5 libxpm4 nginx debhelper libssl-dev libcurl4-gnutls-dev mariadb-server freeradius freeradius-mysql gcc make pkg-config iptables haserl libjson-c-dev gengetopt devscripts libtool bash-completion autoconf automake libffi-dev python python-pip jq)
-PIHOTSPOT_DEPS=( build-essential grep whiptail debconf-utils nfdump figlet git fail2ban hostapd php-mysql php-pear php-gd php-db php-fpm libgd-dev libpcrecpp0v5 libxpm4 nginx debhelper libssl-dev libcurl4-gnutls-dev mariadb-server freeradius freeradius-mysql gcc make pkg-config iptables haserl libjson-c-dev gengetopt devscripts libtool bash-completion autoconf automake libffi-dev python python-pip jq docker docker-compose)
+PIHOTSPOT_DEPS=( build-essential grep whiptail debconf-utils nfdump figlet git fail2ban hostapd php-mysql php-pear php-gd php-db php-fpm libgd-dev libpcrecpp0v5 libxpm4 nginx debhelper libssl-dev libcurl4-gnutls-dev mariadb-server freeradius freeradius-mysql gcc make pkg-config iptables haserl libjson-c-dev gengetopt devscripts libtool bash-completion autoconf automake libffi-dev python python-pip )
 
 install_dependent_packages() {
 
@@ -437,8 +434,6 @@ check_previous_execution() {
     fi
 }
 
-version_gt() { test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1"; }
-
 check_root
 
 check_previous_execution
@@ -470,10 +465,10 @@ else
 	exit 1;
 fi
 
-DEBIAN_VERSION=`cat /etc/debian_version`
-if version_gt $DEBIAN_MINIMAL_VERSION $DEBIAN_VERSION; then
+DEBIAN_VERSION=`cat /etc/*-release | grep VERSION_ID | awk -F= '{print $2}' | sed -e 's/^"//' -e 's/"$//'`
+if [[ $DEBIAN_VERSION -ne 9 ]];then
 	display_message ""
-	display_message "This script is used to get installed on Raspbian Lite (9+)"
+	display_message "This script is used to get installed on Raspbian Stretch Lite"
 	display_message ""
 	exit 1
 fi
@@ -536,13 +531,13 @@ execute_command "echo 'maria-db-$MARIADB_VERSION mysql-server/root_password_agai
 display_message "Getting WAN IP of the Raspberry Pi (for daloradius access)"
 MY_IP=`ifconfig $WAN_INTERFACE | grep "inet " | awk '{ print $2 }'`
 
-display_message "Updating the system hostname to $HOTSPOT_NAME"
-echo $HOTSPOT_NAME > /etc/hostname
-check_returned_code $?
-
 if [[ "$AVAHI_INSTALL" = "Y" ]]; then
     display_message "Adding Avahi dependencies"
     PIHOTSPOT_DEPS+=( avahi-daemon libavahi-client-dev )
+
+    display_message "Updating the system hostname to $HOTSPOT_NAME"
+    echo $HOTSPOT_NAME > /etc/hostname
+    check_returned_code $?
 
     execute_command "grep $HOTSPOT_NAME /etc/hosts" false "Updating /etc/hosts"
     if [[ $COMMAND_RESULT -ne 0 ]]; then
@@ -557,12 +552,12 @@ display_message "Correct configuration for Collectd daemon"
 sed -i "s/^FQDNLookup true$/FQDNLookup false/g" /etc/collectd/collectd.conf
 check_returned_code $?
 
-# type docker 2> /dev/null
-# if [ $? -ne 0 ]; then
-#     display_message "Install Docker"
-#     curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh
-#     check_returned_code $?
-# fi
+type docker 2> /dev/null
+if [ $? -ne 0 ]; then
+    display_message "Install Docker"
+    curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh
+    check_returned_code $?
+fi
 
 id -u kupiki > /dev/null
 if [ $? -ne 0 ]; then
@@ -585,16 +580,12 @@ if [ $? -ne 0 ]; then
     check_returned_code $?
 fi
 
-display_message "Install pika module"
-pip install pika
-check_returned_code $?
-
-# type docker-compose 2> /dev/null
-# if [ $? -ne 0 ]; then
-#     display_message "Install docker-compose"
-#     pip install docker-compose~=1.23.0
-#     check_returned_code $?
-# fi
+type docker-compose 2> /dev/null
+if [ $? -ne 0 ]; then
+    display_message "Install docker-compose"
+    pip install docker-compose
+    check_returned_code $?
+fi
 
 if [[ "$NETFLOW_ENABLED" = "Y" ]]; then
     DEBIAN_FRONTEND=noninteractive apt-get install -y --allow-remove-essential --allow-change-held-packages fprobe nfdump
@@ -785,16 +776,6 @@ if [[ "$KUPIKI_SQL_COUNTERS" = "Y" ]]; then
     execute_command "cp -f /usr/src/sqlcounter /etc/freeradius/3.0/mods-enabled/sqlcounter" true "Adding CoovaChilli counters (limit bandwidth)"
 
     execute_command "chown freerad:freerad /etc/freeradius/3.0/mods-enabled/sqlcounter" true "Updating file access rights"
-
-    allCounters=`cat /etc/freeradius/3.0/mods-enabled/sqlcounter | grep "sqlcounter" | cut -d' ' -f2`
-    positionLine=`grep -m 1 -nr -e "^#.*daily$" /etc/freeradius/3.0/sites-enabled/default | cut -d: -f1`
-
-    display_message "Activating counters"
-    for counterName in $allCounters
-    do
-        sed -n -i "p;${positionLine}a ${counterName}" /etc/freeradius/3.0/sites-enabled/default
-    done
-    check_returned_code $?
 fi
 
 execute_command "freeradius -C" true "Checking freeradius configuration"
@@ -925,12 +906,6 @@ if [[ "$DALORADIUS_INSTALL" = "Y" ]]; then
     display_message "Configuring daloradius DB user password"
     sed -i "s/\$configValues\['CONFIG_DB_PASS'\] = '';/\$configValues\['CONFIG_DB_PASS'\] = 'radpass';/g" /usr/share/nginx/html/daloradius/library/daloradius.conf.php
     check_returned_code $?
-    display_message "Configuring daloradius DB proxy configuration file"
-    sed -i "s/\$configValues\['CONFIG_FILE_RADIUS_PROXY'\] = '\/etc\/freeradius\/proxy.conf';/\$configValues\['CONFIG_FILE_RADIUS_PROXY'\] = '\/etc\/freeradius\/3.0\/proxy.conf';/g" /usr/share/nginx/html/daloradius/library/daloradius.conf.php
-    check_returned_code $?
-    display_message "Configuring daloradius DB proxy path"
-    sed -i "s/\$configValues\['CONFIG_PATH_DALO_VARIABLE_DATA'\] = '\/var\/www\/daloradius\/var';/\$configValues\['CONFIG_PATH_DALO_VARIABLE_DATA'\] = '\/usr\/share\/ngin\/html\/daloradius\/var';/g" /usr/share/nginx/html/daloradius/library/daloradius.conf.php
-    check_returned_code $?
 
     display_message "Building NGINX configuration (default listen port : 80)"
     echo '
@@ -989,10 +964,6 @@ server {
        		fastcgi_pass unix:/var/run/php/php7.0-fpm.sock;
        	}
 }" > /etc/nginx/sites-available/portal
-
-    display_message "Update Chilli configuration to allow HTTPS in iptables"
-    sed -e 's/^HS_TCP_PORTS.*/HS_TCP_PORTS="5000 80 443"/' /etc/chilli/config > /tmp/config && cp /tmp/config /etc/chilli/config
-    check_returned_code $?
 else
     echo "
 server {
@@ -1020,12 +991,8 @@ execute_command "ln -sfT /etc/nginx/sites-available/portal /etc/nginx/sites-enab
 
 execute_command "cp -Rf /usr/src/portal /usr/share/nginx/" true "Installing the portal in Nginx folder"
 
-display_message "Updating Captive Portal configuration file - Register host"
-# sed -i "/XXXXXX/s/XXXXXX/$HOTSPOT_IP/g" /usr/share/nginx/portal/js/configuration.json
-jq --arg hostip $HOTSPOT_IP '.register.host = $hostip' /usr/share/nginx/portal/js/configuration.json > /tmp/configuration.json && cp /tmp/configuration.json /usr/share/nginx/portal/js/configuration.json
-check_returned_code $?
-display_message "Updating Captive Portal configuration file - Chilli Controller host"
-jq --arg hostip $HOTSPOT_IP '.chilliController.host = $hostip' /usr/share/nginx/portal/js/configuration.json > /tmp/configuration.json && cp /tmp/configuration.json /usr/share/nginx/portal/js/configuration.json
+display_message "Updating Captive Portal configuration file"
+sed -i "/XXXXXX/s/XXXXXX/$HOTSPOT_IP/g" /usr/share/nginx/portal/js/configuration.json
 check_returned_code $?
 
 execute_command "nginx -t" true "Checking Nginx configuration file"
@@ -1033,18 +1000,12 @@ execute_command "nginx -t" true "Checking Nginx configuration file"
 execute_command "cp -Rf /usr/src/kupiki-portal-backend /home/kupiki/" true "Installing the portal backend in the user kupiki home folder"
 
 display_message "Updating Captive Portal backend configuration file"
-# sed -i "/pihotspot/s/pihotspot/$MYSQL_PASSWORD/g" /home/kupiki/kupiki-portal-backend/app/src/config.json
-jq --arg password $MYSQL_PASSWORD '.sequelize.freeradius.password = $password' /home/kupiki/kupiki-portal-backend/app/src/config.json > /tmp/config.json && cp /tmp/config.json /home/kupiki/kupiki-portal-backend/app/src/config.json
-check_returned_code $?
-
-display_message "Update rights for NGINX sites"
-chown -R www-data:www-data /usr/share/nginx/*
+sed -i "/pihotspot/s/pihotspot/$MYSQL_PASSWORD/g" /home/kupiki/kupiki-portal-backend/app/src/config.json
 check_returned_code $?
 
 execute_command "chown -R kupiki:kupiki /home/kupiki/kupiki-portal-backend"
 
 display_message "Build the Docker image of Portal backend"
-#su - kupiki -c "cd /home/kupiki/kupiki-portal-backend && /usr/local/bin/docker-compose build"
 su - kupiki -c "cd /home/kupiki/kupiki-portal-backend && /usr/local/bin/docker-compose build"
 check_returned_code $?
 
@@ -1078,9 +1039,6 @@ if [ "$KUPIKI_ALLOW_REGISTER" = "Y" ]; then
     check_returned_code $?
     display_message "Starting Kupiki Portal Backend service"
     /bin/systemctl start kupiki-portal-backend
-    check_returned_code $?
-    display_message "Activating registration in the Portal site"
-    jq '.register.active = true' /usr/share/nginx/portal/js/configuration.json > /tmp/configuration.json && cp /tmp/configuration.json /usr/share/nginx/portal/js/configuration.json
     check_returned_code $?
 fi
 
